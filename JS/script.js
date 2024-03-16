e13677104c3db621a4fd0a8b3dff748fc1d0a97d
lobodxv <manoellobo1@yahoo.com>
2024-03-15 21:46:14 -0300

    Style of bg

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+const bgAnimation = document.getElementById('bgAnimation');
+
+const numberOfColorBoxes = 400;
+
+for (let i = 0; i < numberOfColorBoxes; i++) {
+
+    const colorBox = document.createElement('div');
+    colorBox.classList.add('colorBox');
+    bgAnimation.append(colorBox)
+}
